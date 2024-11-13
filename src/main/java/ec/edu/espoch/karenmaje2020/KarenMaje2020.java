875fffa2b8efe9292a396d2fdc8fc9ed467e0e5a
Karen-404 <tat.3012.mc@gmail.com>
2024-11-13 13:21:36 -0500

    Primer comentario

---
+++ src/main/java/ec/edu/espoch/karenmaje2020/KarenMaje2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.karenmaje2020;
+
+/**
+ *
+ * @author SO-LAB1-PC24
+ */
+public class KarenMaje2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
